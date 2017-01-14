b28900a2bcb38b0c9628251c242fd46c7fde77c9
ByBurton <byburton@outlook.com>
2017-01-15 00:00:32 +0100

    Edited role.lua
    Added some comments. It should explain my reason of some heroes you may find weird in that role / you would put into another role / is missing from a role.
    Added all intelligence heroes.
    Final touches for this night - 12:00 AM in Germany. Good night everybody!

--- role.lua
+++ role.lua
@@ -27,6 +27,7 @@ roles = {
 };
 
 local listHC = {
+--comments by "ByBurton":	I define Hardcarries as heroes that need a lot of money - position 1; usually in safelane and with 1 or 2 supports that help him.
 	"npc_dota_hero_alchemist",
 	"npc_dota_hero_antimage",
 	"npc_dota_hero_arc_warden",
@@ -47,9 +48,12 @@ local listHC = {
 	"npc_dota_hero_monkey_king",
 	"npc_dota_hero_morphling",
 	"npc_dota_hero_naga_siren",
+	"npc_dota_hero_necrolyte",
 	"npc_dota_hero_phantom_assassin",
 	"npc_dota_hero_phantom_lancer",
+	--"npc_dota_hero_queen_of_pain",
 	--"npc_dota_hero_skeleton_king",
+	--"npc_dota_hero_silencer",
 	"npc_dota_hero_slark",
 	"npc_dota_hero_sniper",
 	"npc_dota_hero_spectre",
@@ -57,9 +61,12 @@ local listHC = {
 	"npc_dota_hero_terrorblade",
 	"npc_dota_hero_tiny",
 	"npc_dota_hero_weaver",
+	--"npc_dota_hero_windrunner",
 };
 	
 local listMID = {
+--comments by "ByBurton":	I define Mid as Semicarries. Heroes that need exp and some gold early on, that can easily win lanes 1 vs 1 through skill spamming or harrassing - position 2; always in mid.
+--Have skills that scale bad in early and good into lategame - mostly physical dmg.
 	"npc_dota_hero_abyssal_underlord",
 	"npc_dota_hero_alchemist",
 	"npc_dota_hero_arc_warden",
@@ -75,30 +82,44 @@ local listMID = {
 	"npc_dota_hero_huskar",
 	"npc_dota_hero_invoker",
 	"npc_dota_hero_juggernaut",
+	"npc_dota_hero_leshrac",
 	"npc_dota_hero_lina",
+	--"npc_dota_hero_lion",
 	"npc_dota_hero_magnataur",
 	"npc_dota_hero_medusa",
 	"npc_dota_hero_meepo",
 	"npc_dota_hero_mirana",
 	--"npc_dota_hero_monkey_king",
+	"npc_dota_hero_necrolyte",
 	"npc_dota_hero_nevermore",
 	--"npc_dota_hero_night_stalker",
+	"npc_dota_hero_obsidian_destroyer",
 	"npc_dota_hero_phoenix",
+	"npc_dota_hero_puck",
 	"npc_dota_hero_pudge",
+	"npc_dota_hero_pugna",
+	"npc_dota_hero_queen_of_pain",
 	"npc_dota_hero_rattletrap",
 	"npc_dota_hero_razor",
 	"npc_dota_hero_shadow_fiend",
+	"npc_dota_hero_silencer",
+	--"npc_dota_hero_skywrath_mage",
 	"npc_dota_hero_sniper",
 	"npc_dota_hero_storm_spirit",
 	"npc_dota_hero_templar_assassin",
 	"npc_dota_hero_troll_warlord",
+	"npc_dota_hero_tinker",
 	"npc_dota_hero_tiny",
 	"npc_dota_hero_ursa",
 	"npc_dota_hero_viper",
+	--"npc_dota_hero_warlock",	--He can actually mid, spam the shadow word on the enemy; has good stats, laning phase can be pretty easy. (early bottle).
+	"npc_dota_hero_windrunner",
 	"npc_dota_hero_zuus",
 };
 	
 local listOFF = {
+--comments by "ByBurton":	I define Offlaner as Semicarries too, but a bit less gold focused. They need mostly exp early on, and they play defensiv / use defensive skills to survive their lane 1 vs 2-3 - position 3;
+--always solo or with another support; in offlane aka hardlane. Have abilities that scale good in midgame.
 	"npc_dota_hero_abaddon",
 	"npc_dota_hero_abyssal_underlord",
 	"npc_dota_hero_arc_warden",
@@ -121,6 +142,7 @@ local listOFF = {
 	"npc_dota_hero_elder_titan",
 	"npc_dota_hero_enchantress",
 	"npc_dota_hero_faceless_void",
+	"npc_dota_hero_furion",
 	"npc_dota_hero_huskar",
 	"npc_dota_hero_kunkka",
 	"npc_dota_hero_legion_commander",
@@ -132,17 +154,22 @@ local listOFF = {
 	"npc_dota_hero_mirana",
 	--"npc_dota_hero_monkey_king",
 	"npc_dota_hero_morphling",
+	"npc_dota_hero_necrolyte",
 	"npc_dota_hero_night_stalker",
 	"npc_dota_hero_nyx_assassin",
+	"npc_dota_hero_ogre_magi",
 	"npc_dota_hero_phoenix",
+	"npc_dota_hero_puck",
 	"npc_dota_hero_pudge",
 	"npc_dota_hero_rattletrap",
 	"npc_dota_hero_riki",
 	"npc_dota_hero_sandking",
 	"npc_dota_hero_skeleton_king",
+	--"npc_dota_hero_silencer",
 	"npc_dota_hero_slardar",
 	"npc_dota_hero_spirit_breaker",
 	"npc_dota_hero_sven",
+	"npc_dota_hero_techies", --TECHIES EXIST! ACCEPT IT!
 	"npc_dota_hero_tidehunter",
 	"npc_dota_hero_tiny",
 	"npc_dota_hero_timbersaw",
@@ -152,9 +179,12 @@ local listOFF = {
 	"npc_dota_hero_ursa",
 	"npc_dota_hero_viper",
 	"npc_dota_hero_weaver",
+	--"npc_dota_hero_windrunner",
 };
 
 local listROAMER = {
+--comments by "ByBurton":	I define roaming heroes as heroes who need no or very few gold and levels, and gank lanes to help the team. gets little gold / exp by this.
+--Typically invisible heroes or heroes with a skill to surprise their targets / roam around the map fast - Position 3; Starts usually in the offlane? for some levels to start roaming
 	"npc_dota_hero_bounty_hunter",
 	"npc_dota_hero_bloodseeker",
 	"npc_dota_hero_clinkz",
@@ -165,6 +195,8 @@ local listROAMER = {
 };
 
 local listJUNGLER = {
+--comments by "ByBurton":	I define junglers as heroes, who sacrifice some exp for steady gold income through clearing the jungle early on. They have skills that allow them to do so.
+--Grants the rest of the team more exp, because one hero less on the lane. Can help the safelane by ganking. Requires starting gold to become scary for enemy heroes - position 2 or 3; in jungle.
 	"npc_dota_hero_axe",
 	--"npc_dota_hero_beastmaster",
 	"npc_dota_hero_bloodseeker",
@@ -172,27 +204,41 @@ local listJUNGLER = {
 	"npc_dota_hero_doom_bringer",
 	"npc_dota_hero_enchantress",
 	"npc_dota_hero_enigma",
+	"npc_dota_hero_furion",
 	"npc_dota_hero_legion_commander",
 	"npc_dota_hero_lifestealer",
 	--"npc_dota_hero_lycan",
 };
 	
 local listSEMISUPPORT = {
+--comments by "ByBurton":	I define semi supports as supports who need a little money. they usually do not buy the wards and smokes. Usually have supporting abilities. They help in ganks, and buy other utility items. Require some more
+--gold / exp than full supports - Position 4 - usually helping the offlaner or in a trilane with the safelane carry.
 	"npc_dota_hero_abaddon",
-	"npc_dota_hero_ancient_apparition",
+	--"npc_dota_hero_ancient_apparition",
 	--"npc_dota_hero_arc_warden",
 	"npc_dota_hero_bane",
 	"npc_dota_hero_brewmaster",
+	"npc_dota_hero_chen",
+	--"npc_dota_hero_crystal_maiden",
 	"npc_dota_hero_dazzle",
+	--"npc_dota_hero_disruptor",
 	"npc_dota_hero_earth_spirit",
 	"npc_dota_hero_earthshaker",
 	"npc_dota_hero_elder_titan",
 	"npc_dota_hero_enigma",
 	"npc_dota_hero_jakiro",
+	"npc_dota_hero_leshrac",			--not sure here
+	"npc_dota_hero_lich",
+	"npc_dota_hero_lina",
+	"npc_dota_hero_lion",
+	"npc_dota_hero_ogre_magi",
 	"npc_dota_hero_omniknight",
 	"npc_dota_hero_phoenix",
 	"npc_dota_hero_pudge",
+	"npc_dota_hero_pugna",
 	"npc_dota_hero_sandking",
+	"npc_dota_hero_silencer",
+	"npc_dota_hero_skywrath_mage",
 	"npc_dota_hero_slardar",
 	"npc_dota_hero_tidehunter",
 	"npc_dota_hero_tiny",
@@ -200,11 +246,17 @@ local listSEMISUPPORT = {
 	"npc_dota_hero_tusk",
 	"npc_dota_hero_undying",
 	"npc_dota_hero_vengefulspirit",
-	"npc_dota_hero_venomancer",
+	--"npc_dota_hero_venomancer",
+	"npc_dota_hero_warlock",
+	"npc_dota_hero_windrunner",
+	--"npc_dota_hero_winter_wyvern",
 	"npc_dota_hero_witch_doctor",
+	--"npc_dota_hero_zuus",
 };
 
 local listHARDSUPPORT = {
+--comments by "ByBurton":	I define full supports or hard supports as heroes that hardly need money to be helpful to their carry. Usually possess helpful abilities, stuns, heals. Harass the enemy offlaner out
+--of the lane / exp range. No money, little exp - Position 5; usually in the safelane protecting its carry. stacking and pulling.
 	"npc_dota_hero_abaddon",
 	"npc_dota_hero_ancient_apparition",
 	"npc_dota_hero_bane",
@@ -213,15 +265,23 @@ local listHARDSUPPORT = {
 	"npc_dota_hero_disruptor",
 	"npc_dota_hero_earthshaker",
 	"npc_dota_hero_jakiro",
+	"npc_dota_hero_keeper_of_the_light",
+	"npc_dota_hero_lich",
 	"npc_dota_hero_lion",
 	"npc_dota_hero_omniknight",
+	"npc_dota_hero_oracle",
+	"npc_dota_hero_rubick",
 	"npc_dota_hero_sandking",
 	"npc_dota_hero_shadow_demon",
+	"npc_dota_hero_shadow_shaman",
+	"npc_dota_hero_skywrath_mage",
 	"npc_dota_hero_tidehunter",
 	"npc_dota_hero_treant_protector",
 	"npc_dota_hero_undying",
+	"npc_dota_hero_visage",
 	"npc_dota_hero_vengefulspirit",
 	"npc_dota_hero_venomancer",
+	"npc_dota_hero_winter_wyvern",
 	"npc_dota_hero_wisp",
 };
 	

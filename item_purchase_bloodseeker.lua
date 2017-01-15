a1dc915dde9d9d37543d140a850afbda0ac3577d
Keithen <Keithen.Neu@gmail.com>
2017-01-15 22:29:56 +0100

    more stuff to buy

--- item_purchase_bloodseeker.lua
+++ item_purchase_bloodseeker.lua
@@ -12,7 +12,11 @@ local tableItemsToBuyAsJungler = {
 	"item_branches",
 	"item_branches",
 	"item_ring_of_protection",
-	"item_recipe_iron_talon"
+	"item_recipe_iron_talon",
+	"item_boots",
+	"items_gloves",
+	"item_belt_of_strength",
+	"item_recipe_power_treads"
 };
 
 ----------------------------------------------------------------------------------------------------

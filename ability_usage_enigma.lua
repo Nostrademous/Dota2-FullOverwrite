0345c7921536c983f2f96b08f8a3f7590743f16b
Keithen <Keithen.Neu@gmail.com>
2017-01-16 17:57:49 +0100

    keep enigma files here.

---
+++ ability_usage_enigma.lua
@@ -0,0 +1,16 @@
+-------------------------------------------------------------------------------
+--- AUTHOR: Keithen
+--- GITHUB REPO: https://github.com/Nostrademous/Dota2-FullOverwrite
+-------------------------------------------------------------------------------
+
+_G._savedEnv = getfenv()
+module( "ability_usage_enigma", package.seeall )
+
+function AbilityUsageThink()
+	if ( GetGameState() ~= GAME_STATE_GAME_IN_PROGRESS and GetGameState() ~= GAME_STATE_PRE_GAME ) then return end
+
+	local npcBot = GetBot();
+
+end
+
+for k,v in pairs( ability_usage_enigma ) do _G._savedEnv[k] = v end

660027698439267fd28165b13b86d05536d4fb5e
Andrzej <nostrademous@hotmail.com>
2017-01-19 08:06:04 -0500

    added Init function to Item Purchasing and working referencing of global hero data

--- item_purchase_generic_test.lua
+++ item_purchase_generic_test.lua
@@ -16,6 +16,20 @@ local gHeroVar = require( GetScriptDirectory().."/global_hero_data" )
 --]]
 
 -------------------------------------------------------------------------------
+-- Helper Functions for accessing Global Hero Data
+-------------------------------------------------------------------------------
+
+function setHeroVar(var, value)
+	local bot = GetBot()
+	gHeroVar.SetVar(bot:GetPlayerID(), var, value)
+end
+
+function getHeroVar(var)
+	local bot = GetBot()
+	return gHeroVar.GetVar(bot:GetPlayerID(), var)
+end
+
+-------------------------------------------------------------------------------
 -- Declarations
 -------------------------------------------------------------------------------
 
@@ -91,7 +105,10 @@ function X:Think(npcBot)
 
 	-- If game not in progress bail
 	if ( GetGameState() ~= GAME_STATE_GAME_IN_PROGRESS and GetGameState() ~= GAME_STATE_PRE_GAME ) then return end
-
+	
+	-- Initialization
+	self:Init(npcBot)
+	
 	-- If there's an item to be purchased already bail
 	if ( (npcBot:GetNextItemPurchaseValue() > 0) and (npcBot:GetGold() < npcBot:GetNextItemPurchaseValue()) ) then
 		return
@@ -143,8 +160,16 @@ function X:Think(npcBot)
 end
 
 -------------------------------------------------------------------------------
--- Utilitly functions
+-- Utility functions
 -------------------------------------------------------------------------------
+
+function X:Init(npcBot)
+	local bInit = getHeroVar("ItemPurchaseInitialized")
+	if bInit == nil then
+		print(getHeroVar("Name"), " - Initializing Item Purchase class - Role: ", getHeroVar("Role"))
+		setHeroVar("ItemPurchaseInitialized", true)
+	end
+end
 
 function X:UpdatePurchaseOrder(npcBot)
 	-- Core (doesn't buy utility items such as wards)

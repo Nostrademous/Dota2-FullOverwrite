9e66e96972b8cb64b05b5046112d5082e678c158
Andrzej <nostrademous@hotmail.com>
2017-01-28 12:59:51 -0500

    tabs/spaces; return if we using ability

--- jungling_generic.lua
+++ jungling_generic.lua
@@ -14,13 +14,13 @@ require( GetScriptDirectory().."/jungle_status")
 local gHeroVar = require( GetScriptDirectory().."/global_hero_data" )
 
 function setHeroVar(var, value)
-	local bot = GetBot()
-	gHeroVar.SetVar(bot:GetPlayerID(), var, value)
+    local bot = GetBot()
+    gHeroVar.SetVar(bot:GetPlayerID(), var, value)
 end
 
 function getHeroVar(var)
-	local bot = GetBot()
-	return gHeroVar.GetVar(bot:GetPlayerID(), var)
+    local bot = GetBot()
+    return gHeroVar.GetVar(bot:GetPlayerID(), var)
 end
 ----------
 
@@ -35,41 +35,41 @@ local DamageThreshold=1.0;
 local MoveThreshold=1.0;
 
 local JunglingStates={
-	FindCamp=0,
-	MoveToCamp=1,
-	WaitForSpawn=2,
-	Stack=3,
-	CleanCamp=4
+    FindCamp=0,
+    MoveToCamp=1,
+    WaitForSpawn=2,
+    Stack=3,
+    CleanCamp=4
 }
 
 function OnStart(npcBot)
-	setHeroVar("JunglingState", JunglingStates.FindCamp)
-	setHeroVar("move_ticks", 0)
-	-- TODO: if there are camps, consider tp'ing to the jungle
+    setHeroVar("JunglingState", JunglingStates.FindCamp)
+    setHeroVar("move_ticks", 0)
+    -- TODO: if there are camps, consider tp'ing to the jungle
 
-	-- TODO: Pickup runes
-	-- TODO: help lanes
-	-- TODO: when to stop jungling? (NEVER!!)
+    -- TODO: Pickup runes
+    -- TODO: help lanes
+    -- TODO: when to stop jungling? (NEVER!!)
 end
 
 function OnResume(bot)
-	utils.myPrint("resume jungling")
-	setHeroVar("JunglingState", JunglingStates.FindCamp) -- reset state
+    utils.myPrint("resume jungling")
+    setHeroVar("JunglingState", JunglingStates.FindCamp) -- reset state
 end
 
 ----------------------------------
 
 local function FindCamp(bot)
-	-- TODO: just killing the closest one might not be the best strategy
-	local jungle = jungle_status.GetJungle(GetTeam()) or {}
-	local maxcamplvl = getHeroVar("Self"):GetMaxClearableCampLevel(bot)
-	jungle = FindCampsByMaxDifficulty(jungle, maxcamplvl)
-	if #jungle == 0 then -- they are all dead
-		jungle = utils.deepcopy(utils.tableNeutralCamps[GetTeam()])
-		jungle = FindCampsByMaxDifficulty(jungle, maxcamplvl)
-	end
-	local camp, camp2 = utils.NearestNeutralCamp(bot, jungle)
-    
+    -- TODO: just killing the closest one might not be the best strategy
+    local jungle = jungle_status.GetJungle(GetTeam()) or {}
+    local maxcamplvl = getHeroVar("Self"):GetMaxClearableCampLevel(bot)
+    jungle = FindCampsByMaxDifficulty(jungle, maxcamplvl)
+    if #jungle == 0 then -- they are all dead
+        jungle = utils.deepcopy(utils.tableNeutralCamps[GetTeam()])
+        jungle = FindCampsByMaxDifficulty(jungle, maxcamplvl)
+    end
+    local camp, camp2 = utils.NearestNeutralCamp(bot, jungle)
+
     if camp2 ~= nil then
         local listAllies = GetUnitList(UNIT_LIST_ALLIED_HEROES)
         for _, ally in pairs(listAllies) do
@@ -83,105 +83,105 @@ local function FindCamp(bot)
             end
         end
     end
-    
-	if getHeroVar("currentCamp") == nil or camp[constants.VECTOR] ~= getHeroVar("currentCamp")[constants.VECTOR] then
-		print(utils.GetHeroName(bot), "moves to camp")
-	end
-	setHeroVar("currentCamp", camp)
-	setHeroVar("move_ticks", 0)
-	setHeroVar("JunglingState", JunglingStates.MoveToCamp)
+
+    if getHeroVar("currentCamp") == nil or camp[constants.VECTOR] ~= getHeroVar("currentCamp")[constants.VECTOR] then
+        print(utils.GetHeroName(bot), "moves to camp")
+    end
+    setHeroVar("currentCamp", camp)
+    setHeroVar("move_ticks", 0)
+    setHeroVar("JunglingState", JunglingStates.MoveToCamp)
 end
 
 local function MoveToCamp(bot)
-	if getHeroVar("currentCamp") == nil then
-		setHeroVar("JunglingState", JunglingStates.FindCamp)
-		return
-	end
-	if GetUnitToLocationDistance(bot, getHeroVar("currentCamp")[constants.VECTOR]) > 200 then
-		local ticks = getHeroVar("move_ticks")
-		if ticks > 50 then -- don't do this every frame
-			setHeroVar("JunglingState", JunglingStates.FindCamp) -- crossing the jungle takes a lot of time. Check for camps that may have spawned
-			return
-		else
-			setHeroVar("move_ticks", ticks + 1)
-		end
-		bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.VECTOR])
-		return
-	end
-	local neutrals = bot:GetNearbyCreeps(EyeRange, true)
-	if #neutrals == 0 then -- no creeps here
-		local jungle = jungle_status.GetJungle(GetTeam()) or {}
-		jungle = FindCampsByMaxDifficulty(jungle, getHeroVar("Self"):GetMaxClearableCampLevel(bot))
-		if #jungle == 0 then -- jungle is empty
-			setHeroVar("waituntil", utils.NextNeutralSpawn())
-			utils.myPrint("waits for spawn")
-			setHeroVar("JunglingState", JunglingStates.WaitForSpawn)
-		else
-			utils.myPrint("No creeps here :(") -- one of   dumb me, dumb teammates, blocked by enemy, farmed by enemy
-			jungle_status.JungleCampClear(GetTeam(), getHeroVar("currentCamp")[constants.VECTOR])
-			utils.myPrint("finds camp")
-			setHeroVar("JunglingState", JunglingStates.FindCamp)
-		end
-	else
-		--print(utils.GetHeroName(bot), "KILLS")
-		setHeroVar("JunglingState", JunglingStates.CleanCamp)
-	end
+    if getHeroVar("currentCamp") == nil then
+        setHeroVar("JunglingState", JunglingStates.FindCamp)
+        return
+    end
+    if GetUnitToLocationDistance(bot, getHeroVar("currentCamp")[constants.VECTOR]) > 200 then
+        local ticks = getHeroVar("move_ticks")
+        if ticks > 50 then -- don't do this every frame
+            setHeroVar("JunglingState", JunglingStates.FindCamp) -- crossing the jungle takes a lot of time. Check for camps that may have spawned
+            return
+        else
+            setHeroVar("move_ticks", ticks + 1)
+        end
+        bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.VECTOR])
+        return
+    end
+    local neutrals = bot:GetNearbyCreeps(EyeRange, true)
+    if #neutrals == 0 then -- no creeps here
+        local jungle = jungle_status.GetJungle(GetTeam()) or {}
+        jungle = FindCampsByMaxDifficulty(jungle, getHeroVar("Self"):GetMaxClearableCampLevel(bot))
+        if #jungle == 0 then -- jungle is empty
+            setHeroVar("waituntil", utils.NextNeutralSpawn())
+            utils.myPrint("waits for spawn")
+            setHeroVar("JunglingState", JunglingStates.WaitForSpawn)
+        else
+            utils.myPrint("No creeps here :(") -- one of   dumb me, dumb teammates, blocked by enemy, farmed by enemy
+            jungle_status.JungleCampClear(GetTeam(), getHeroVar("currentCamp")[constants.VECTOR])
+            utils.myPrint("finds camp")
+            setHeroVar("JunglingState", JunglingStates.FindCamp)
+        end
+    else
+        --print(utils.GetHeroName(bot), "KILLS")
+        setHeroVar("JunglingState", JunglingStates.CleanCamp)
+    end
 end
 
 local function WaitForSpawn(bot)
-	if DotaTime() < getHeroVar("waituntil") then
-		bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.STACK_VECTOR]) -- TODO: use a vector that is closer to the camp
-		return
-	end
-	setHeroVar("JunglingState", JunglingStates.MoveToCamp)
+    if DotaTime() < getHeroVar("waituntil") then
+        bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.STACK_VECTOR]) -- TODO: use a vector that is closer to the camp
+        return
+    end
+    setHeroVar("JunglingState", JunglingStates.MoveToCamp)
 end
 
 local function Stack(bot)
-	if DotaTime() < getHeroVar("waituntil") then
-		bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.STACK_VECTOR])
-		return
-	end
-	setHeroVar("JunglingState", JunglingStates.FindCamp)
+    if DotaTime() < getHeroVar("waituntil") then
+        bot:Action_MoveToLocation(getHeroVar("currentCamp")[constants.STACK_VECTOR])
+        return
+    end
+    setHeroVar("JunglingState", JunglingStates.FindCamp)
 end
 
 local function CleanCamp(bot)
-	-- TODO: make sure we have aggro when attempting to stack
-	-- TODO: don't attack enemy creeps, unless they attack us / make sure we stay in jungle
-	-- TODO: instead of stacking, could we just kill them and move ou of the camp?
-	-- TODO: make sure we can actually kill the camp.
-    
-	local dtime = DotaTime() % 120
-	local stacktime = getHeroVar("currentCamp")[constants.STACK_TIME]
-	if dtime >= stacktime and dtime <= stacktime + 1 then
-		setHeroVar("JunglingState", JunglingStates.Stack)
-		utils.myPrint("stacks")
-		setHeroVar("waituntil", utils.NextNeutralSpawn())
-		return
-	end
-	local neutrals = bot:GetNearbyCreeps(EyeRange,true)
-	if #neutrals == 0 then -- we did it
-		local camp, _ = utils.NearestNeutralCamp(bot, jungle_status.GetJungle(GetTeam())) -- we might not have killed the `currentCamp`
+    -- TODO: make sure we have aggro when attempting to stack
+    -- TODO: don't attack enemy creeps, unless they attack us / make sure we stay in jungle
+    -- TODO: instead of stacking, could we just kill them and move ou of the camp?
+    -- TODO: make sure we can actually kill the camp.
+
+    local dtime = DotaTime() % 120
+    local stacktime = getHeroVar("currentCamp")[constants.STACK_TIME]
+    if dtime >= stacktime and dtime <= stacktime + 1 then
+        setHeroVar("JunglingState", JunglingStates.Stack)
+        utils.myPrint("stacks")
+        setHeroVar("waituntil", utils.NextNeutralSpawn())
+        return
+    end
+    local neutrals = bot:GetNearbyCreeps(EyeRange,true)
+    if #neutrals == 0 then -- we did it
+        local camp, _ = utils.NearestNeutralCamp(bot, jungle_status.GetJungle(GetTeam())) -- we might not have killed the `currentCamp`
         -- we could have been killing lane creeps, don't mistaken for neutral
         if GetUnitToLocationDistance(bot, camp[constants.VECTOR]) <= 200 then
             jungle_status.JungleCampClear(GetTeam(), camp[constants.VECTOR])
         end
-		utils.myPrint("finds camp")
-		setHeroVar("JunglingState", JunglingStates.FindCamp)
-	else
-		getHeroVar("Self"):DoCleanCamp(bot, neutrals)
-	end
+        utils.myPrint("finds camp")
+        setHeroVar("JunglingState", JunglingStates.FindCamp)
+    else
+        getHeroVar("Self"):DoCleanCamp(bot, neutrals)
+    end
 end
 
 ----------------------------------
 
 function FindCampsByMaxDifficulty(jungle, difficulty)
-	result = {}
-	for i,camp in pairs(jungle) do
-		if camp[constants.DIFFICULTY] <= difficulty then
-			result[#result+1] = camp
-		end
-	end
-	return result
+    result = {}
+    for i,camp in pairs(jungle) do
+        if camp[constants.DIFFICULTY] <= difficulty then
+            result[#result+1] = camp
+        end
+    end
+    return result
 end
 
 ----------------------------------
@@ -199,12 +199,13 @@ local States = {
 function Think(npcBot)
     --[[
     local me = getHeroVar("Self")
-	if me:getPrevAction() ~= ACTION_JUNGLING then
-		OnResume(npcBot)
-	end
+    if me:getPrevAction() ~= ACTION_JUNGLING then
+        OnResume(npcBot)
+    end
     --]]
+    if npcBot:IsUsingAbility() then return end
 
-	States[getHeroVar("JunglingState")](npcBot)
+    States[getHeroVar("JunglingState")](npcBot)
 end
 
 

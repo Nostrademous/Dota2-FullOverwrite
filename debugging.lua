5deb432de6fa90256244c4dc972a4cab4d2e664a
Keithen <Keithen.Neu@gmail.com>
2017-03-02 12:02:35 +0100

    add circles to debugging

--- debugging.lua
+++ debugging.lua
@@ -12,6 +12,7 @@ local last_draw_time = -500
 
 local bot_states = {}
 local team_states = {}
+local circles = {}
 
 local LINE_HEIGHT = 10
 local TITLE_VALUE_DELTA_X = 10
@@ -24,6 +25,7 @@ local TEAM_STATES_MAX_LINES = 6
 local TEAM_STATES_X = 1550
 local TEAM_STATES_Y = 400
 
+-- gets called by the framework
 function draw()
     if last_draw_time > GameTime() - 0.010 then return end -- TODO: check actual frame time
     last_draw_time = GameTime()
@@ -45,8 +47,14 @@ function draw()
         end
         y = y + (TEAM_STATES_MAX_LINES + 1) * LINE_HEIGHT
     end
+
+    for name, circle in pairs(circles) do
+        -- TODO: draw name? (kinda hard to do, but could be nice)
+        DebugDrawCircle( circle.center, circle.radius, circle.r, circle.g, circle.b )
+    end
 end
 
+-- set a line in the specified bot's text area
 function SetBotState(bot, line, state)
     if line < 1 or BOT_STATES_MAX_LINES > 2 then
         print("SetBotState: line out of bounds!")
@@ -59,6 +67,7 @@ function SetBotState(bot, line, state)
     bot_states[name][line] = state
 end
 
+-- set a line in the specified category's text area
 function SetTeamState(category, line, state)
     if line < 1 or line > TEAM_STATES_MAX_LINES then
         print("SetBotState: line out of bounds!")
@@ -68,6 +77,18 @@ function SetTeamState(category, line, state)
         team_states[category] = {}
     end
     team_states[category][line] = state
+end
+
+-- draw a (filled) circle
+function SetCircle(name, center, r, g, b, radius)
+    if radius == nil then radius = 50 end
+
+    circles[name] = {["center"] = center, ["r"] = r, ["g"] = g, ["b"] = b, ["radius"] = radius}
+end
+
+-- remove a circle
+function DeleteCircle(name)
+    circles[name] = nil
 end
 
 for k,v in pairs( debugging ) do _G._savedEnv[k] = v end
